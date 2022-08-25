f55cbb99790b08ba3098f42e877d2abe62350be4
Francisca Villavicencio <fran.villavicencioa@gmail.com>
2022-08-24 23:46:44 -0400

    ejercicios con jquery

---
+++ script.js
@@ -0,0 +1,15 @@
+//lo que dice esta sentencia es: selecciono el documento, cuando este listo ejecuta la función
+
+$( document ).ready(function() {
+    alert("web cargada");
+    //aqui van todas las acciones que yo quiero realizar con las partes del documento
+    
+    //sintaxis de la accion en jquery
+    //el selector me permite seleccionar la parte del documento sobre el que quiero realizar una acción
+
+    /* $(selector).accion() */
+});
+
+//las funciones pueden ir afuera
+
+
